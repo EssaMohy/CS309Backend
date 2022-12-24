69318619755d6cd55b32ed376a3a795ac0784dc2
Allaa Azzam <allaa3zzaam@gmail.com>
2022-12-25 01:02:57 +0200

    token

---
+++ middleware/auth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function(res,req,next){
+
+//get token from header
+
+const token = req.header('x-auth-toke');
+
+//check if no token
+
+if(!token){
+
+    return res.status (401).json({msg: 'No token, Authorization denied'});
+}
+
+//verify token
+
+try {
+    const decoded = jwt.verify(token, config.get('jwtSecret')); 
+
+    req.user = decoded.user;
+    next();
+} catch (error) {
+    res.status(401).json({msg: 'Token is not valid'});
+}
+
+
+
+}
