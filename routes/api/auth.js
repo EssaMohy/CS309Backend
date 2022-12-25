d34c8de9fc63f3cba940f74acf866c658691b78a
Allaa Azzam <allaa3zzaam@gmail.com>
2022-12-25 02:30:47 +0200

    Update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 
 //@route  GET api/auth
 //@desc   auth route
 //@access Public
-router.get(`/`, auth, (req, res) => res.send('auth route'));
+router.get(`/`, auth, async(req,res)=>{
+
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (error) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+
+}
+);
+
 
 module.exports = router;
