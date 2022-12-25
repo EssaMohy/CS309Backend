299b834f9db12b5560676e5406f00985485084bf
Allaa Azzam <allaa3zzaam@gmail.com>
2022-12-25 03:02:44 +0200

    Auth

--- Server.js
+++ Server.js
@@ -16,4 +16,4 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`I LOVE YOU:${PORT}`));
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
+const { check, validationResulst } = require("express-validator");
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 //@route  GET api/auth
 //@desc   auth route
 //@access Public
-router.get(`/`, auth, async(req,res)=>{
+router.get(`/`, auth, async (req, res) => {
 
     try {
         const user = await User.findById(req.user.id).select('-password');
@@ -19,5 +23,59 @@ router.get(`/`, auth, async(req,res)=>{
 }
 );
 
+//@route  Post api/users
+//@desc   Authinicate user & get token
+//@access Public
+router.post('/', [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password',"Password is required").exists(),
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password } = req.body;
+    try {
+
+        //See if user doesn't exist
+
+
+        let user = await User.findOne({ email });
+        if (!user) {
+            res.status(400).json({ errors: [{ msg: "Invalid Information" }] });
+        }
+
+
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch){
+            res.status(400).json({ errors: [{ msg: "Invalid Information" }] })
+        }
+
+
+        //Return jsonwebtoken
+
+        const payload = {
+
+            user: {
+                id: user.id,
+            }
+        }
+        jwt.sign(payload,
+            config.get('jwtSecret'),
+            { expiresIn: 90000000 },
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token })
+
+            }
+        );
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server error");
+    }
+});
 
 module.exports = router;
