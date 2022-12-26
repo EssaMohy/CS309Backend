0fa45cc8792170ae4ee1dc96593bc0097c2704c3
Allaa Azzam <78929333+allaa3zzaam@users.noreply.github.com>
2022-12-26 23:47:09 +0200

    Update verifyToken.js

--- routes/api/verifyToken.js
+++ routes/api/verifyToken.js
@@ -1,40 +1,6 @@
 const jwt = require("jsonwebtoken");
-//creating token for each user
+
 const verifyToken = (req, res, next) => {
-<<<<<<< HEAD
-  const authHeader = req.headers.token;
-  if (authHeader) {
-    const token = authHeader.split(" ")[1];
-    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
-      if (err) res.status(403).json("Token is not valid!");
-      req.user = user;
-      next();
-    });
-  } else {
-    return res.status(401).json("You are not authenticated!");
-  }
-};
-//Check if can make changes
-const verifyTokenAndAuthorization = (req, res, next) => {
-  verifyToken(req, res, () => {
-    if (req.user.id === req.params.id || req.user.isAdmin) {
-      next();
-    } else {
-      res.status(403).json("You can't make that change");
-    }
-  });
-};
-//Check if an Admin
-const verifyTokenAndAdmin = (req, res, next) => {
-  verifyToken(req, res, () => {
-    if (req.user.isAdmin) {
-      next();
-    } else {
-      res.status(403).json("Sorry, you're not an admin");
-    }
-  });
-};
-=======
 
     const authHeader = req.headers.token;
 
@@ -51,6 +17,7 @@ const verifyTokenAndAdmin = (req, res, next) => {
         return res.status(401).json("You are not authonicated");
     }
 }
+//Checking if can make changes
 const verifyTokenAndAuthorization = (req, res, next) => {
     verifyToken(req, res, () => {
         if (req.user.id === req.params.id || req.user.isAdmin) {
@@ -60,6 +27,7 @@ const verifyTokenAndAuthorization = (req, res, next) => {
         }
     })
 }
+//Check if an admin
 const verifyTokenAndِAdmin = (req, res, next) => {
     verifyToken(req, res, () => {
         if (req.user.isAdmin) {
@@ -69,13 +37,6 @@ const verifyTokenAndِAdmin = (req, res, next) => {
         }
     })
 }
-
 module.exports = { verifyToken, verifyTokenAndAuthorization, verifyTokenAndِAdmin };
 
->>>>>>> 06b59997cf1ea243d466a02083f7acf15a73d18e
 
-module.exports = {
-  verifyToken,
-  verifyTokenAndAuthorization,
-  verifyTokenAndAdmin,
-};
