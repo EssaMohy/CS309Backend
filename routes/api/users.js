c03f1ed948103af5bd4124c933ff711f1ced87e7
Allaa Azzam <78929333+allaa3zzaam@users.noreply.github.com>
2022-12-26 23:47:51 +0200

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,21 +1,15 @@
 const express = require('express');
 const User = require("../../models/user");
 const { findByIdAndUpdate } = require('../../models/user');
-<<<<<<< HEAD
-const {verifyToken, verifyTokenAndAuthorization,verifyTokenAndAdmin} = require("./verifyToken");
-=======
 const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndِAdmin } = require("./verifyToken");
->>>>>>> 06b59997cf1ea243d466a02083f7acf15a73d18e
 const router = express.Router();
-const dotenv = require("dotenv");
 //@route  GET api/users
 //@desc   users route
 //@access Public
 
 
 //UPDATE USER
-<<<<<<< HEAD
-//Checking if the user can make changes (auth or Admin)
+//Checking if the user can make changes
 router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
     if (req.body.password) {
       req.body.password = CryptoJS.AES.encrypt(
@@ -23,8 +17,7 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
         process.env.PASS_SEC
       ).toString();
     }
-
-    //Finding user by its ID
+  
     try {
       const updatedUser = await User.findByIdAndUpdate(
         req.params.id,
@@ -33,31 +26,12 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
         },
         { new: true }
       );
-      //Returning the updated info for user
       res.status(200).json(updatedUser);
     } catch (err) {
       res.status(500).json(err);
     }
   });
-=======
-router.put("./:id", verifyTokenAndAuthorization, async (req, res) => {
-    if (req.body.password) {
-        req.body.password = CryptoJS.AES.encrypt(req.body.password, 'Secret Stuff');
-    }
-    try {
-        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-            $set: req.body
-        }, { new: true });
-        res.status(200).json(updatedUser);
-    }
-    catch (err) {
-        res.status(500).json(err);
-    }
-});
->>>>>>> 06b59997cf1ea243d466a02083f7acf15a73d18e
 
 //lh:5000//api/users;
 
 module.exports = router;
-
-
